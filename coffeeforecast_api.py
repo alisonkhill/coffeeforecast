1c1abc3c6dc937b1072aad102af774f7e78a2fe5
Alison Hill <alison.hill@aya.yale.edu>
2022-03-21 20:50:09 -0400

    Removed link to config file with API Keys and added code for user to input API keys

--- coffeeforecast_api.py
+++ coffeeforecast_api.py
@@ -1,21 +1,25 @@
 # Import necessary packages
-import config
 import requests
 import pandas as pd
 import datetime
 
 # Get zip code from user
-zip = input('What is your zip code?\n')
+zip = input('Greetings, coffee drinker! What is your zip code?\n')
+LOCATION_API_KEY = input('Please enter a positionstack API Key. You can request one for free at https://positionstack.com/signup/free \n')
 
 # Convert zip code to latitude and longitude using positionstack API
-LOCATION_URL = 'http://api.positionstack.com/v1/forward?access_key='+config.LOCATION_API_KEY+'&query='+zip+'&country=US&output=json'
+LOCATION_URL = 'http://api.positionstack.com/v1/forward?access_key='+LOCATION_API_KEY+'&query='+zip+'&country=US&output=json'
 location_request = requests.get(LOCATION_URL)
 location_json = location_request.json()
 lat = str(location_json.get('data')[0].get('latitude'))
 lon = str(location_json.get('data')[0].get('longitude'))
 
+print('Your coordinates are ' + lat + ', ' + lon + '\n')
+
+WEATHER_API_KEY = input('Please enter an OpenWeatherMap API Key. You can request one for free at https://home.openweathermap.org/users/sign_up \n')
+
 # Get temperature and conditions from open weather map API using latitude and longitude
-WEATHER_URL = 'https://api.openweathermap.org/data/2.5/onecall?&lat='+lat+'&lon='+lon+'&exclude=minutely,hourly&appid='+config.WEATHER_API_KEY+'&units=imperial'
+WEATHER_URL = 'https://api.openweathermap.org/data/2.5/onecall?&lat='+lat+'&lon='+lon+'&exclude=minutely,hourly&appid='+WEATHER_API_KEY+'&units=imperial'
 weather_request = requests.get(WEATHER_URL)
 weather_json = weather_request.json()
 
@@ -49,4 +53,5 @@ for x in range(0,7):
 
 # Output as table using pandas module    
 df = pd.DataFrame.from_dict(forecast, orient = 'index', columns = ['Coffee Order','Feels Like', 'Conditions'])
+print('Here is your coffee forecast. Please caffeinate responsibly.\n')
 print(df)
