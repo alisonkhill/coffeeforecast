b27d26e668764b7231840e55f6087d1c54799a2a
Alison Hill <alison.hill@aya.yale.edu>
2022-03-29 14:19:26 -0400

    Adding function parameters and main() function wrap

--- coffeeforecast.py
+++ coffeeforecast.py
@@ -17,32 +17,33 @@ def get_zip():
 
 
 # Obtain positionstack API Key from user, output coordinates
-def get_location():
+def get_location(zip_code):
     while True:
         try:
             location_api_key = input('Please enter a positionstack API Key. You can request one for free at https://positionstack.com/signup/free \n')
-            LOCATION_URL = 'http://api.positionstack.com/v1/forward?access_key='+location_api_key+'&query='+user_zip+'&country=US&output=json'
+            LOCATION_URL = 'http://api.positionstack.com/v1/forward?access_key='+location_api_key+'&query='+zip_code+'&country=US&output=json'
             location_request = requests.get(LOCATION_URL)
             location_json = location_request.json()
-            lat = str(location_json.get('data')[0].get('latitude'))
-            lon = str(location_json.get('data')[0].get('longitude'))
+            latitude = str(location_json.get('data')[0].get('latitude'))
+            longitude = str(location_json.get('data')[0].get('longitude'))
         except TypeError:
             print('Your positionstack API Key was unsuccessful.')
             continue
         else:
             break
-    return lat, lon
+    return latitude, longitude
 
 
 # Obtain OpenWeatherMap API key from user, input coordinates, output temperature and conditions
-def get_weather():
+def get_weather_data(lat, lon):
     while True:
         try:
             weather_api_key = input('Please enter an OpenWeatherMap API Key. You can request one for free at https://home.openweathermap.org/users/sign_up \n')
-            WEATHER_URL = 'https://api.openweathermap.org/data/2.5/onecall?&lat='+coordinates[0]+'&lon='+coordinates[1]+'&exclude=minutely,hourly&appid='+weather_api_key+'&units=imperial'
+            WEATHER_URL = 'https://api.openweathermap.org/data/2.5/onecall?&lat='+lat+'&lon='+lon+'&exclude=minutely,hourly&appid='+weather_api_key+'&units=imperial'
             weather_request = requests.get(WEATHER_URL)
             weather_json = weather_request.json()
-            offset = str(weather_json.get('daily')[0].get('sunrise'))
+            # Check that API key works by accessing sample data point in json
+            test_api_key = str(weather_json.get('daily')[0].get('sunrise'))
         except TypeError:
             print('Your OpenWeatherMap API Key was unsuccessful.')
             continue
@@ -52,11 +53,10 @@ def get_weather():
 
 
 # Create lists of variables for graph
-def get_forecast():
+def get_forecast(weather):
     # Create empty lists for dates, relative temperatures, and conditions
     days = []
     rel_temps_F = []
-    rel_temps_C = []
     conditions = []        
     # Loop through the next 7 days and populate lists
     for x in range(0,7):
@@ -65,19 +65,18 @@ def get_forecast():
         # Convert date to strftime
         days.append(datetime.datetime.utcfromtimestamp(dt).strftime('%a %d %b'))
         rel_temps_F.append(round(weather.get('daily')[x].get('feels_like').get('day')))
-        rel_temps_C.append(round((rel_temps_F[x] - 32) * (5/9)))
         conditions.append(weather.get('daily')[x].get('weather')[0].get('main'))
-    return days, rel_temps_F, rel_temps_C, conditions
+    return days, rel_temps_F, conditions
 
 
 # Recommend Hot or Iced based on relative temperature and conditions
-def recommendation():
+def recommendation(forecast):
     recs = []
     for x in range(0,7):
         if forecast[1][x] < 50:
             recs.append('Hot')
-        elif 60 > forecast[1][x] > 50:
-            if forecast[3][x] == 'Clear':
+        elif 60 > forecast[1][x] >= 50:
+            if forecast[2][x] == 'Clear':
                 recs.append('Iced')
             else:
                 recs.append('Hot')
@@ -87,9 +86,9 @@ def recommendation():
 
 
 # Assign conditions to colors for bar graph display
-def assign_colors(): 
+def assign_colors(forecast): 
     conditions_colors = []
-    for condition in forecast[3]:
+    for condition in forecast[2]:
         if condition == "Clouds" or condition == 'Fog':
             conditions_colors.append('grey')
         elif condition == 'Clear':
@@ -100,14 +99,14 @@ def assign_colors():
             conditions_colors.append('whitesmoke')
         else:
             conditions_colors.append('black')
-    return(conditions_colors)
+    return conditions_colors
 
 
 # Plot bar chart with table data below
-def make_chart():
+def make_chart(forecast, colors, coffees):
     plt.figure(figsize=(10,10))
     plt.bar(forecast[0], forecast[1], color = colors, edgecolor = 'black')
-    plt.table(cellText=[forecast[2], forecast[3], coffees], rowLabels=['Feels Like, Deg (C)','Conditions', 'Coffee Order'], cellLoc='center',bbox=[0.0,-0.45,1,0.28])
+    plt.table(cellText=[forecast[1], forecast[2], coffees], rowLabels=['Feels Like, Deg (F)','Conditions', 'Coffee Order'], cellLoc='center',bbox=[0.0,-0.45,1,0.28])
     plt.title('Coffee Forecast')
     plt.ylabel('Feels Like, Deg (F)')
     plt.xticks(rotation=45)
@@ -115,17 +114,17 @@ def make_chart():
     plt.show()
 
 
-# Calling functions
-print('Greetings, coffee drinker!')
-user_zip = get_zip()
-coordinates = get_location()
-weather = get_weather()
-forecast = get_forecast()
-coffees = recommendation()
-colors = assign_colors()
-chart = make_chart()
-print('Please caffeinate responsibly.\n')
+# Creating main function for program
+def main():
+    print('Greetings, coffee drinker!')
+    user_zip = get_zip()
+    lat, lon = get_location(user_zip)
+    weather = get_weather_data(lat, lon)
+    forecast = get_forecast(weather)
+    coffees = recommendation(forecast)
+    colors = assign_colors(forecast)
+    make_chart(forecast, colors, coffees)
+    print('Please caffeinate responsibly.\n')
 
-
-
-
+# Calling main function
+main()
